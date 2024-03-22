9689f359bd419cb08e41d9e696d58c5010f246e0
salvo <you@example.com>
2024-03-21 17:14:34 -1200

    salvo

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById('btns1').addEventListener('click', () => {
+   alert('Clicked')
+})
+
+let btn2 = document.getElementById('btns2').addEventListener('click', () => {
+   alert('Clicked')
+})
